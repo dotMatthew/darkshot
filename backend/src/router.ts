369d05e6a9626648468b5e692bd7021acdc96dcc
Mathias Dollenbacher <hello@mdollenbacher.net>
2021-05-31 14:18:41 +0200

    change wrong controller export

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { StatusController } from './controller/Status.controller';
 import { UploadController } from './controller/Upload.controller';
 
 const router: Router = Router();
 
+router.use("/", StatusController)
 router.use("/post-picture", UploadController);
 
-export const MainRouter: Router = Router();
+export const MainRouter: Router = router;
