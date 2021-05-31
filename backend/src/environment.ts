9de672d54b00153f45b8b3d87f3758753502d86c
Mathias Dollenbacher <hello@mdollenbacher.net>
2021-05-31 16:54:43 +0200

    add default environment

--- backend/src/environment.ts
+++ backend/src/environment.ts
@@ -5,11 +5,13 @@ dotenv.config();
 interface Environment {
     port: number | string;
     mongodb_uri: string;
+    data_storage: string;
 }
 
 const environment: Environment = {
     port: process.env.PORT || 8080,
     mongodb_uri: process.env.MONGODB_URI || '',
+    data_storage: process.env.DATA_STORAGE || '/tmp/',
 };
 
 export default environment;
