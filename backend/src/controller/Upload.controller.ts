b84a1860b421edb369c172bccf4f5ca291acc9bf
Mathias Dollenbacher <hello@mdollenbacher.net>
2021-05-31 14:42:44 +0200

    remove body-parser

--- backend/src/controller/Upload.controller.ts
+++ backend/src/controller/Upload.controller.ts
@@ -3,7 +3,7 @@ import { Router, Request } from "express";
 const router: Router = Router();
 
 router.post("/", async (req: Request, res) => {
-    console.log("Got body ", req.body);
+    console.log('Got body', req.body);
     res.sendStatus(200);
 });
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import environment from './environment';
 import { MainRouter } from './router';
-import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(bodyParser.json);
-
+app.use(express.json());
 app.use("/", MainRouter);
 
 app.listen(environment.port, () => {
